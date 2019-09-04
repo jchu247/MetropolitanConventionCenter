8e339dab647956a877d7930cbb1b7170241ec575
606786 <joechu@C02YX0Z1LVDT.local>
2019-09-04 15:41:50 -0400

    Added Docker Files

---
+++ customer-service-gradle/src/main/java/io/mcc/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package io.mcc.customerservice.repository;
+
+import io.mcc.customerservice.domain.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Customer findCustomerByName(String name);
+
+
+    @Override
+    Iterable<Customer> findAllById(Iterable<Long> iterable);
+}
